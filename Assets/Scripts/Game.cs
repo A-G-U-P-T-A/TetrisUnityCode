3ab1e2f21b96eac639450ca3bd66ac80646ecaa2
HellBender1018 <hell.bender.g.1018@gmail.com>
2020-04-25 20:35:59 +0530

    init changes

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static int gridHeight = 20;
+    public static int gridWidth = 10;
+    public static Transform[,] grid = new Transform[gridWidth,gridHeight]; 
+
+    void Start()
+    {
+        SpawnNextTetramino();    
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void updateGrid(Tetramino tetramino)
+    {
+        for(int y = 0; y < gridHeight; y++)
+        {
+            for(int x=0; x < gridWidth; x++) 
+            {
+                if (grid[x, y] != null)
+                {
+                    if (grid[x, y].parent == tetramino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+        }
+    }
+
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+
+    public Vector2 round(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    public void SpawnNextTetramino()
+    {
+        GameObject nextTetramino = (GameObject)Instantiate(
+            Resources.Load(getRandomTetramino(), 
+            typeof(GameObject)), 
+            new Vector2(5.0f, 20.0f), 
+            Quaternion.identity);
+    }
+
+    string getRandomTetramino()
+    {
+        int randomTetromino = Random.Range(1, 8);
+        
+        switch (randomTetromino)
+        {
+            case 1:
+                return "Prefabs/tetramino_J";
+            case 2:
+                return "Prefabs/tetramino_L";
+            case 3:
+                return "Prefabs/tetramino_Long";
+            case 4:
+                return "Prefabs/tetramino_S";
+            case 5:
+                return "Prefabs/tetramino_Square";
+            case 6:
+                return "Prefabs/tetramino_T";
+            case 7:
+                return "Prefabs/tetramino_Z";
+            default:
+                return "Prefabs/tetramino_T";
+        }
+    }
+}
---
+++ Assets/Scripts/Tetramino.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetramino : MonoBehaviour
+{
+    float fall = 0;
+    public float fall_speed = 1;
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += new Vector3(1, 0, 0);
+            if (CheckIsValidPosition()){}
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-1, 0, 0);
+            if (CheckIsValidPosition()){}
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (allowRotation)
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+                if (CheckIsValidPosition()) { }
+                else
+                {
+                    if (limitRotation)
+                    {
+                        if (transform.rotation.eulerAngles.z >= 90)
+                            transform.Rotate(0, 0, -90);
+                        else
+                            transform.Rotate(0, 0, 90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                }
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)|| Time.time-fall >= fall_speed) {
+            transform.position += new Vector3(0, -1, 0);
+            if (CheckIsValidPosition()) { }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+                enabled = false;
+                FindObjectOfType<Game>().SpawnNextTetramino();
+            }
+            fall = Time.time;
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach(Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().round(mino.position);
+            if (!FindObjectOfType<Game>().CheckIsInsideGrid(pos))
+            {
+                return false;
+            }
+            else
+            {
+
+            }
+        }
+        return true;
+    }
+}
